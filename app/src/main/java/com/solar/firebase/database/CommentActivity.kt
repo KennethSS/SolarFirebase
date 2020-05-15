250d2365040e1335b2cbb06e5549e91977876196
kenneth <wlsrhkd4023@gmail.com>
2020-05-15 20:53:48 +0900

    Add to FirebaseStorage upload function

--- app/src/main/java/com/solar/firebase/database/CommentActivity.kt
+++ app/src/main/java/com/solar/firebase/database/CommentActivity.kt
@@ -1,21 +1,36 @@
 package com.solar.firebase.database
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.google.firebase.database.FirebaseDatabase
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.sample.basefirebase.database.FirebaseDatabaseManager
+import com.sample.basefirebase.database.FirebaseStorageManager
 import com.sample.basefirebase.database.recyclerview.FirebaseRecyclerViewAdapter
 import com.solar.firebase.R
 import com.solar.firebase.database.entity.comment.Comment
 import com.solar.firebase.database.entity.comment.CommentViewHolder
 import com.solar.firebase.extension.inflate
 import com.solar.solarview.activity.BaseActivity
+import com.zhihu.matisse.Matisse
+import com.zhihu.matisse.MimeType
+import com.zhihu.matisse.engine.impl.GlideEngine
 import kotlinx.android.synthetic.main.activity_firebase_database.*
+import kotlinx.android.synthetic.main.item_comment.*
 import kotlinx.android.synthetic.main.item_comment.view.*
 
 class CommentActivity : BaseActivity() {
     var adapter: FirebaseRecyclerAdapter<Comment, CommentViewHolder>? = null
+    private var uri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firebase_database)
@@ -28,9 +43,64 @@ class CommentActivity : BaseActivity() {
             }
         }
 
+        upload_img.setOnClickListener {
+            uri?.let {
+                showProgress()
+                FirebaseStorageManager.upload("test", it) {
+                    Log.d("CommentActivity", it.toString())
+                    dismissProgress()
+                }
+            }
+
+        }
+        get_image.setOnClickListener {
+            TedPermission.with(this)
+                    .setPermissionListener(object: PermissionListener {
+                        override fun onPermissionGranted() {
+                            Matisse.from(this@CommentActivity)
+                                    .choose(MimeType.ofImage())
+                                    .countable(true)
+                                    .maxSelectable(1)
+                                    //.addFilter(GifSizeFilter(320, 320, 5 * Filter.K * Filter.K))
+                                    //.gridExpectedSize(resources.getDimensionPixelSize(R.dimen.grid_expected_size))
+                                    .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)
+                                    .thumbnailScale(0.85f)
+                                    .imageEngine(GlideEngine())
+                                    //.showPreview(false) // Default is `true
+                                    .theme(R.style.Matisse_Dracula)
+                                    .forResult(0)
+                        }
+
+                        override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                        }
+
+                    })
+                    .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                    .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    .check()
+        }
+
+
         loadCommentList("comments", 50)
 
         //FirebaseDatabase.getInstance().getReference()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0) {
+            if (resultCode == Activity.RESULT_OK) {
+                val images = Matisse.obtainResult(data)
+
+                if (images.isNotEmpty()) {
+                    this.uri = images[0]
+                    Glide.with(comment_img)
+                            .load(images[0])
+                            .into(comment_img)
+                }
+            }
+        }
     }
 
     override fun onStart() {
---
+++ basefirebase/src/main/java/com/sample/basefirebase/database/FirebaseStorageManager.kt
@@ -0,0 +1,32 @@
+package com.sample.basefirebase.database
+
+import android.net.Uri
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
+
+object FirebaseStorageManager {
+    fun upload(reference: String, uri: Uri, result: (uri: Uri?) -> Unit) {
+        val storageReference = FirebaseStorage
+                .getInstance()
+                .getReference(reference)
+
+        storageReference.putFile(uri)
+                .continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                    if (!task.isSuccessful) {
+                        task.exception?.let {
+                            throw it
+                        }
+                    }
+                    return@Continuation storageReference.downloadUrl
+                })
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        result(task.result)
+                    } else {
+                        result(null)
+                    }
+                }
+    }
+}
