f8b1dea5419a946545ec31d10caeadebd58c1862
kenneth <wlsrhkd4023@gmail.com>
2020-06-27 11:27:52 +0900

    modify: Add Firebase Storage primary key

--- basefirebase/src/main/java/com/sample/basefirebase/database/FirebaseStorageManager.kt
+++ basefirebase/src/main/java/com/sample/basefirebase/database/FirebaseStorageManager.kt
@@ -5,12 +5,14 @@ import com.google.android.gms.tasks.Continuation
 import com.google.android.gms.tasks.Task
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.UploadTask
+import java.util.*
 
 object FirebaseStorageManager {
     fun upload(reference: String, uri: Uri, result: (uri: Uri?) -> Unit) {
         val storageReference = FirebaseStorage
                 .getInstance()
                 .getReference(reference)
+                .child(UUID.randomUUID().toString())
 
         storageReference.putFile(uri)
                 .continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
